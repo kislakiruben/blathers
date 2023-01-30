842a0fb22289c7c30285d1e16b467819af56164e
Ruben <ruben@kislaki.net>
2023-01-30 18:07:06 +0200

    installed body-parser

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
-import Express from "express";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import Express from "express";
 
 dotenv.config();
 
 const app: Express.Application = Express();
 const port = process.env.SERVER_PORT;
 
+app.use(bodyParser.json());
 app.get("/", (req: Express.Request, res: Express.Response) => {
   res.send("hello world");
 });
