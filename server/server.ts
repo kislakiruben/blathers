d3d4a15f97684b72764546d6c56c7acb58365c92
Ruben <ruben@kislaki.net>
2023-02-02 00:10:40 +0200

    added logging on the express server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import Express from "express";
+import winston from "winston";
+import expressWinston from "express-winston";
 
 import nftGet from "./routes/nftGet";
 
@@ -11,7 +13,27 @@ const port = process.env.PORT || process.env.SERVER_PORT;
 
 app.use(bodyParser.json());
 app.use(Express.static("dist"));
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    expressFormat: true,
+    colorize: true,
+  })
+);
 app.get("/api/nft", nftGet);
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
